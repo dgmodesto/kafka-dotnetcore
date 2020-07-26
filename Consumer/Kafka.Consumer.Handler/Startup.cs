1d2b955fd3c47973d5eb54d261a1c66f63db577d
Douglas Gomes Modesto <d.modesto@guide.com.br>
2020-07-26 10:58:51 -0300

    feat(project): criar projeto dotnetcore para utilização do kafka

---
+++ Consumer/Kafka.Consumer.Handler/Startup.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kafka.Consumer.Handler.Handlers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Kafka.Consumer.Handler
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            
+            services.AddTransient<IHostedService, MessageHandler>();
+
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            ILogger logger = loggerFactory.CreateLogger<Program>();
+            logger.LogInformation("#######################################################");
+            logger.LogInformation("Iniciando Aplicação Consumer");
+            logger.LogInformation("#######################################################");
+
+            app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
+        }
+    }
+}
---
+++ Producer/Kafka.Producer.API/Controllers/ProducerController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kafka.Producer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProducerController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(string), 201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public IActionResult Post([FromQuery] string message)
+        {
+            return Created("", SendMessageKafka(message));
+        }
+
+        private string SendMessageKafka(string message)
+        {
+
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+            
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+
+                    var sendResult = producer
+                                        .ProduceAsync("fila_pedido", new Message<Null, string> { Value = message })
+                                            .GetAwaiter()
+                                                .GetResult();
+
+                }
+                catch (ProduceException<Null, string> ex)
+                {
+
+                    Console.WriteLine($"Delivery failed: { ex.Error.Reason}");
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
